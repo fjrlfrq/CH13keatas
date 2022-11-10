747763eccfae6d3361fb29ee689859cacebe7a80
Luthfi <luthfisandi20@gmail.com>
2022-11-10 12:58:37 +0700

    challenge14

--- CH14/query.sql
+++ CH14/query.sql
@@ -1,24 +1,43 @@
 CREATE TABLE Mahasiswa (
-    nim varchar(100) primary key not null, 
-    nama varchar(100) not null, 
+    nim varchar(100) PRIMARY KEY NOT NULL, 
+    nama varchar(100) NOT NULL, 
     alamat text
 );
 
 CREATE TABLE Dosen (
-    iddosen varchar(100) primary key not null, 
-    nama varchar(100) not null
+    iddosen varchar(100) PRIMARY KEY NOT NULL, 
+    nama varchar(100) NOT NULL
 );
 
 CREATE TABLE Jurusan (
-    idjurusan varchar(100) primary key not null, 
-    nama varchar(100) not null
+    idjurusan varchar(100) PRIMARY KEY NOT NULL, 
+    nama varchar(100) NOT NULL
 );
 
 CREATE TABLE MataKuliah (
-    idmatkul varchar(100) primary key not null,  
-    nama varchar(100) not null , 
-    sks numeric not null
+    idmatkul varchar(100) PRIMARY KEY NOT NULL,  
+    nama varchar(100) NOT NULL , 
+    sks numeric NOT NULL
 );
+
+CREATE TABLE Pengajar (
+    id integer PRIMARY KEY AUTOINCREMENT, 
+    iddosen varchar(100) NOT NULL, 
+    idjurusan varchar(100) NOT NULL, 
+    idmatkul varchar(100) NOT NULL, 
+    FOREIGN KEY(iddosen) REFERENCES Dosen(iddosen), 
+    FOREIGN KEY(idjurusan) REFERENCES Jurusan(idjurusan), 
+    FOREIGN KEY(idmatkul) REFERENCES MataKuliah(idmatkul)
+);
+
+CREATE TABLE Pelajar (
+    id integer PRIMARY KEY AUTOINCREMENT, 
+    nim varchar(100) NOT NULL, 
+    idjurusan varchar(100) NOT NULL, 
+    idmatkul varchar(100) NOT NULL, 
+    FOREIGN KEY(nim) REFERENCES Mahasiswa(nim), 
+    FOREIGN KEY(idjurusan) REFERENCES Jurusan(idjurusan), 
+    FOREIGN KEY(idmatkul) REFERENCES MataKuliah(idmatkul));
 
 INSERT INTO Mahasiswa VALUES('A0001','Salsabilla Syifatunnisa','Padang'),
                             ('A0002','Muhammad Fajar Al-Faruq','Bandung'),
@@ -33,4 +52,16 @@ INSERT INTO Jurusan VALUES('00001','Informatika'),
                           ('00002','Seni dan Desain'),
                           ('00003','Hukum');
 
-INSERT INTO MataKuliah VALUES('A001','Matematika','3'),('A002','Seni Budaya','3'),('A003','Agama','2');
+INSERT INTO MataKuliah VALUES('A001','Matematika','3'),
+                             ('A002','Seni Budaya','3'),
+                             ('A003','Agama','2');
+
+INSERT INTO Pengajar (iddosen, idjurusan, idmatkul) VALUES('0001A', '00001', 'A001'),
+                                                          ('0002A', '00002', 'A002'),
+                                                          ('0003A', '00003', 'A003'),
+                                                          ('0002A', '00002', 'A002');
+
+INSERT INTO Pelajar (nim, idjurusan, idmatkul) VALUES('A0001', '00001', 'A001'),
+                                                     ('A0002', '00002', 'A002'),
+                                                     ('A0003', '00003', 'A003'),
+                                                     ('A0002', '00002', 'A002');
